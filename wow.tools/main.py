4dc8f4fc3eee0ff00cb2dd410b7aab5e63d5bac3
Ferronn <ferronn@ferronn.dev>
2021-01-31 11:41:49 -0600

    rewrite wow tools import to use new mechanisms

--- wow.tools/main.py
+++ wow.tools/main.py
@@ -1,103 +1,71 @@
 import base64
-import bs4
 import json
-import pprint
+import bs4
 import requests
-from google.cloud import bigquery
 from google.cloud import pubsub_v1
 from google.cloud import storage
 
 print('connecting to bigquery...')
-bq = bigquery.Client('wow-ferronn-dev')
+storage_client = storage.Client()
+bucket = storage_client.bucket('wowdb-import-stage')
+ps = pubsub_v1.PublisherClient()
+
+def fetch(url, params=None):
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response
 
 def latest_classic_version():
-    data = requests.get('https://wow.tools/dbc/')
-    data.raise_for_status()
-    soup = bs4.BeautifulSoup(data.text, 'html.parser')
+    data = fetch('https://wow.tools/dbc/').text
+    soup = bs4.BeautifulSoup(data, 'html.parser')
     for v in soup.find('select', id='lockedBuild').find_all('option'):
         s = v.string.split(' ', 1)
         if len(s) == 2 and s[1] == '(ClassicRetail)':
             return s[0]
     raise Exception('no version')
 
-def get_blob(name, build):
-    client = storage.Client()
-    bucket = client.bucket('wow.ferronn.dev')
-    return bucket.blob(f'wow.tools/dbc/{build}/{name}.csv')
-
-def pull_csv(name, build):
-    base = 'https://wow.tools/dbc/api/export/'
-    data = requests.get(f'{base}?name={name}&build={build}&locale=enUS')
-    data.raise_for_status()
-    return data.text
-
-def pull_schema(name, build):
-    base = 'https://wow.tools/dbc/api/header'
-    data = requests.get(f'{base}/{name}/?build={build}')
-    data.raise_for_status()
-    return data.json()
-
-def pull_db(build):
-    base = 'https://api.wow.tools/databases'
-    data = requests.get(f'{base}/{build}')
-    data.raise_for_status()
-    return data.json()
-
-def get_dataset(v):
-    return bq.create_dataset('wow-ferronn-dev.wow_tools_dbc_' + v.replace('.', '_'), exists_ok=True)
-
-def http_publish(req):
+def http_publish(_):
     print('fetching classic version...')
     v = latest_classic_version()
     print('using classic version', v)
-    ds = get_dataset(v)
     print('getting wow.tools table list...')
-    dbcs = set(e['name'] for e in pull_db(v))
-    print('listing bigquery tables...')
-    bqt = set(t.table_id for t in bq.list_tables(ds))
-    ps = pubsub_v1.PublisherClient()
+    dbcs = set(
+        e['name']
+        for e in fetch(f'https://api.wow.tools/databases/{v}').json())
     topic = ps.topic_path('wow-ferronn-dev', 'wow-tools-dbc')
     futures = [
         ps.publish(topic, json.dumps({'v': v, 'dbc': dbc}).encode('ascii'))
-        for dbc in (dbcs - bqt)
+        for dbc in dbcs
     ]
     results = [f.result() for f in futures]
     msg = f'published {len(results)} messages'
     print(msg)
     return msg
 
-def pubsub_dbc(event, ctx):
+def pubsub_dbc(event, _):
     data = json.loads(base64.b64decode(event['data']))
     v = data['v']
     dbc = data['dbc']
     print('using classic version', v)
     print('working on', dbc)
-    b = get_blob(dbc, v)
+    b = bucket.blob('wow_tools_dbc_' + v.replace('.', '_') + f'/{dbc}.csv')
     if b.exists():
         print('blob already exists')
-    else:
-        print('fetching csv...')
-        d = pull_csv(dbc, v)
-        print('uploading csv...')
-        b.upload_from_string(d)
+        return
     print('fetching schema...')
-    schema = [
-        bigquery.schema.SchemaField(
-            name=h.replace('[', '_').replace(']', '_').lower(),
-            field_type='STRING',
-        ) for h in pull_schema(dbc, v)['headers']
-    ]
-    pprint.pprint(schema)
-    print('creating bq table...')
-    ds = get_dataset(v)
-    job = bq.load_table_from_uri(
-        f'gs://wow.ferronn.dev/wow.tools/dbc/{v}/{dbc}.csv',
-        ds.table(dbc),
-        job_config=bigquery.job.LoadJobConfig(
-            allow_quoted_newlines=True,
-            autodetect=False,
-            schema=schema,
-            skip_leading_rows=1,
-            write_disposition='WRITE_EMPTY'))
-    job.result()
+    b.metadata['schema'] = json.dumps([
+        header.replace('[', '_').replace(']', '_').lower()
+        for header in fetch(
+            f'https://wow.tools/dbc/api/header/{dbc}/',
+            {'build': v},
+        ).json()['headers']
+    ])
+    print('fetching csv...')
+    csv = fetch('https://wow.tools/dbc/api/export/', {
+        'name': dbc,
+        'build': v,
+        'locale': 'enUS',
+    }).text
+    print('uploading csv...')
+    b.upload_from_string(csv)
     print('all done with', dbc)
